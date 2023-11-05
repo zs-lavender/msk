1a5ec1d1f0258ea662e3e9ad9bb6da74387bfbcd
zs-lavender <149854899+zs-lavender@users.noreply.github.com>
2023-11-05 21:35:51 +0800

    Add files via upload

---
+++ rasp2/rasp_send.py
@@ -0,0 +1,53 @@
+# coding: utf-8
+# by maorio
+# * 发送数据给其他树莓派
+import get_self_gps
+import socket
+import time
+
+# 集成了Socket通信常用函数的类
+class connect_Raspberry():
+    def __init__(self,host,port):
+        print("客户端开启")
+        # 套接字接口
+        self.mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # 设置ip和端口
+
+        try:
+            self.mySocket.connect((host, port))  #连接到服务器
+            print("连接到服务器")
+        except:  #连接不成功，运行最初的ip
+            print('连接RASP不成功')
+
+    def send(self, gps):
+        # 发送消息
+        print('1',str(gps[0]), str(gps[1]))
+        msg = str(gps[0]) + "#" + str(gps[1])
+        # 编码发送
+        self.mySocket.send(msg.encode("utf-8"))
+        print("成功发送消息")
+
+    def rev(self):
+        # 接收消息
+        msg = self.mySocket.recv(1024)
+        msg = msg.decode('utf-8')
+        # 返回确认消息
+        print('接收到电脑发送的Ok', msg)
+        return msg
+
+    def close(self):
+        # 关闭连接
+        self.mySocket.close()
+        print("与树莓派连接中断\n")
+        exit()
+
+
+# 设置ip和端口(IP为树莓派的IP地址)
+myRaspConnection = connect_Raspberry('192.168.137.100', 1471)
+print(get_self_gps.get_gps())
+myRaspConnection.send(get_self_gps.get_gps())
+while True:
+    # 在接收到确认信息后再进行下一次发送    
+    if myRaspConnection.rev():
+        time.sleep(1)
+        myRaspConnection.send(get_self_gps.get_gps())
---
+++ rasp2/voice_communicate.py
@@ -0,0 +1,52 @@
+# coding: utf-8
+# by ljc
+# * 语音通信
+import subprocess
+import multiprocessing
+
+# 发送进程
+def send_process(command):
+    print("Currently on the send end")
+    p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    for line in p.stdout:
+        print(line)
+
+# 接收进程
+def recv_process(command):
+    print("Currently on the recv end")
+    p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    for line in p.stdout:
+        print(line)
+
+def card_get():
+    p = subprocess.Popen('arecord -l', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    for line in p.stdout:
+        if len(line) > 60:
+            for i in line:
+                if i>48 and i<=57:
+                    return i-48
+                
+if __name__ == "__main__":
+    command1 = "arecord -D plughw:" + str(card_get()) + ",0 -t wav -f cd -r 8000 | nc 192.168.137.237 8888"
+    command2 = "nc -l -p 8888 | aplay"
+    # 定义进程
+    send_proc = multiprocessing.Process(target=send_process,args = (command1,))
+    recv_proc = multiprocessing.Process(target=recv_process,args = (command2,))
+    recv_proc.start()
+    # 接收模式还是发送模式
+    while True:
+        line = input("plz input your choice:\n")
+        if line.lower() == "s":
+            recv_proc.terminate()  
+            send_proc = multiprocessing.Process(target=send_process,args = (command1,))
+            send_proc.start()
+            print("recv end closed, send end opened")
+
+        elif line.lower() == "q":
+            send_proc.terminate()  
+            recv_proc = multiprocessing.Process(target=recv_process,args = (command2,))
+            recv_proc.start()
+            print("send end closed, recv end opened")
+
+        else:
+            print("plz input the right choice!")
